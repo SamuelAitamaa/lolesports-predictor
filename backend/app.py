717410ead5fb8fb04a44b34b5f9f73ed4f7ecb11
samueai <samuel.aitamaa@hotmail.com>
2022-02-22 18:15:57 +0200

    Initial commit

---
+++ backend/app.py
@@ -0,0 +1,31 @@
+import os
+from flask import Flask, render_template, request
+from reverseProxy import proxyRequest
+from classifier import predict
+import time
+import sys
+
+
+MODE = os.getenv('FLASK_ENV')
+DEV_SERVER_URL = 'http://localhost:3000/'
+
+app = Flask(__name__)
+
+# Ignore static folder in development mode.
+if MODE == "development":
+    app = Flask(__name__, static_folder=None)
+
+@app.route('/')
+@app.route('/<path:path>')
+def index(path=''):
+    if MODE == 'development':
+        return proxyRequest(DEV_SERVER_URL, path)
+    else:
+        return render_template("index.html")
+
+@app.route('/', methods = ['POST'])
+def get_query_from_react():
+    data = request.get_json()
+    result = predict(data.get("team1"), data.get("team2"), data.get("region"))
+    sys.stdout.flush()
+    return result
